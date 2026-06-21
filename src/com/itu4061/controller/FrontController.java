81bee87755501f41888fee93f8777780b98aeb80
karl2719 <ravohitsoakarl@gmail.com>
2026-06-22 01:07:48 +0300

    Ajout de la gestion des packages dans FrontController et amélioration de l'affichage des URL

--- src/com/itu4061/controller/FrontController.java
+++ src/com/itu4061/controller/FrontController.java
@@ -5,8 +5,21 @@ import jakarta.servlet.http.*;
 
 import java.io.File;
 import java.io.PrintWriter;
+import java.util.List;
+import java.util.Map;
 
 public class FrontController extends HttpServlet {
+
+    protected Map<String, Class<?>> classAnnote;
+    protected Package[] packages;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        packages = classLoader.getDefinedPackages();
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, java.io.IOException {
@@ -32,7 +45,15 @@ public class FrontController extends HttpServlet {
             out.println("<title>test</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>GET ALL URL</h1>"+ url.toString()) ;
+            out.println("<h1>GET ALL URL : </h1>" + url.toString() + "</br>" );
+            out.println(Thread.currentThread().getContextClassLoader().toString() );
+            out.println("<ul>");
+            for (Package pack : packages) {
+                out.println(pack.getName());
+                out.println("</br>");
+            }
+            out.println("</ul>");
+        
             out.println("</body>");
             out.println("</html>");
 
