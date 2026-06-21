a2389c7260f9e0087cb4dd77241398bf4a5d9f90
karl2719 <ravohitsoakarl@gmail.com>
2026-06-22 01:35:30 +0300

    wqwq

---
+++ src/com/itu4061/annotation/Annotation.java
@@ -0,0 +1,13 @@
+package com.itu4061.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+//test package
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Annotation {
+    String type() ;
+}
